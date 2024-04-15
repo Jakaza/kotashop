0f41a326ba45754d12e3e210b83c0ce4c9a42812
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-04-15 14:28:26 +0200

    Initial commit

---
+++ kotashop/src/test/java/za/ac/tut/kotashop/KotashopApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.tut.kotashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
