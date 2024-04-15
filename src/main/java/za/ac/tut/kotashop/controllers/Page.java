f005eb339c59440c358f9664fe0fc07f6ac69732
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-04-15 15:39:26 +0200

    Controller Main Page

---
+++ src/main/java/za/ac/tut/kotashop/controllers/Page.java
@@ -0,0 +1,26 @@
+package za.ac.tut.kotashop.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Page {
+
+
+    @GetMapping("/")
+    private String homePage(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    private String loginPage(){
+        return "login";
+    }
+
+
+    @GetMapping("/*")
+    private String pageNotFound(){
+        return "404";
+    }
+}
